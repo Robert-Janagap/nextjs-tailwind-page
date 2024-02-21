54210434b174f575e6ad7301210f6739d4baf27a
Robert Janagap <janagaprobert@gmail.com>
2024-02-21 13:05:27 +0800

    add popular product

--- components/PopularProducts.tsx
+++ components/PopularProducts.tsx
@@ -1,6 +1,19 @@
+import { products } from "@/constants"
+import PopularProductCard from "./PopularProductCard"
+
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold"><span className="text-coral-red">Popular</span> Products</h2>
+        <p className="lg:max-w-lg font-montserrat text-slate-gray">experience top not quality and style with our selections</p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-col-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product}/>
+        ))}
+      </div>
+    </section>
   )
 }
 

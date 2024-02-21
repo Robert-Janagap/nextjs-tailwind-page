ea411097907d1cc72f22909efb735ca885ff08c8
Robert Janagap <janagaprobert@gmail.com>
2024-02-21 14:55:31 +0800

    review card

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image"
 
-const Button = ({label, iconUrl}) => {
+const Button = ({label, iconUrl, backgroundColor, textColor, borderColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full text-white border-coral-red bg-coral-red">{label}
-    <Image src={iconUrl} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none 
+    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'text-white border-coral-red bg-coral-red'} rounded-full`}>{label}
+    {iconUrl && 
+      <Image src={iconUrl} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>
+    }
     </button>
   )
 }
